3d423d3130e83a8c503c60d3c1cc338c1ee453fa
chimmanikrishna <153270527+chimmanikrishna@users.noreply.github.com>
2025-12-30 16:47:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Everyone from Google Cloud Run-Happy Learning! ****HAPPY NEW YEAR UDAY****"
+    return "Hello Everyone from Google Cloud Run-Happy Learning! ****HAPPY NEW YEAR SONU AND MY LOVELY KID****"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
